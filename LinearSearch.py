58b309aca5f87c3aa81f7c0bd43a0cfc4a33d96d
kreinmuth <67114977+kreinmuth@users.noreply.github.com>
2021-12-17 19:26:58 -0800

    Update LinearSearch.py

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,10 +1,10 @@
 
 
-#How it works, the algorithm searchs through a list to find a matching variable
+#How it works, the algorithm searchs through a list one by one to find a matching variable
 
 
 def linearsearch(string_list, x):
-   for i in range(len(string_list)): #Loops through the list from 0 t the max list length
+   for i in range(len(string_list)): #Loops through the list from 0 t the max list length, put reveresed in front range to go in opposite order, can use all range functions to change how it searches
       if string_list[i] == x:
          return i #Returns the location of the matching variable
    return -1# This is the value the function will return tht can be used to compare it
